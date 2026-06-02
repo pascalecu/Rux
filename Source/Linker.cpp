68fc1447b4e3b39a7da0727d08330eda955bc183
Ștefan-Iulian Alecu <165364995+pascalecu@users.noreply.github.com>
2026-06-02 04:10:03 +0300

    Move linker utils to Utils.h

--- Source/Linker.cpp
+++ Source/Linker.cpp
@@ -6,6 +6,7 @@
 
 #include "Rux/Linker.h"
 
+#include "Rux/Linker/Utils.h"
 #include "Rux/Platform.h"
 
 #include <algorithm>
@@ -39,65 +40,6 @@ namespace Rux {
 
     using namespace Platform;
     namespace fs = std::filesystem;
-
-
-    namespace Detail {
-        template <typename T, typename... U>
-        concept NoneOf = (!std::same_as<T, U> && ...);
-
-        template <typename T>
-        concept EndianConvertible =
-            std::integral<T> && NoneOf<std::remove_cv_t<T>, bool, char, char8_t, char16_t, char32_t, wchar_t>;
-
-        template <EndianConvertible T>
-        [[nodiscard]]
-        constexpr T ByteSwapIfBigEndian(T value) {
-            if constexpr (HostEndianness == Endian::Big) {
-                return std::byteswap(value);
-            }
-
-            return value;
-        }
-
-        [[nodiscard]]
-        constexpr size_t AlignUp(size_t value, size_t alignment) {
-            assert(alignment != 0);
-
-            if (std::has_single_bit(alignment)) {
-                return (value + alignment - 1) & ~(alignment - 1);
-            }
-
-            return ((value + alignment - 1) / alignment) * alignment;
-        }
-
-        template <typename T>
-        concept TriviallySerializable = std::is_trivially_copyable_v<T> && !std::is_pointer_v<T>;
-    } // namespace Detail
-
-    namespace Bytes {
-        constexpr std::byte B(std::integral auto v) noexcept {
-            return std::byte{static_cast<std::uint8_t>(v)};
-        }
-
-        template <std::integral... Ts>
-        constexpr std::array<std::byte, sizeof...(Ts)> Pack(Ts... v) noexcept {
-            return {std::byte{static_cast<std::uint8_t>(v)}...};
-        }
-
-        inline std::span<const std::byte> FromString(std::string_view s) noexcept {
-            return std::as_bytes(std::span{s});
-        }
-
-        template <std::size_t N>
-        constexpr std::span<const std::byte, N> FromArray(const std::array<std::byte, N>& a) noexcept {
-            return std::span{a};
-        }
-
-        template <std::integral... Ts>
-        constexpr auto PackSpan(Ts... v) {
-            return Pack(v...); // usable directly as span
-        }
-    } // namespace Bytes
 
     class BinaryWriter {
     public:
